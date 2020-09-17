28fe94fc4e29af74141e26c5d0cc3fd9ed0d33cb
victor-athum <victor@athum.com>
2020-09-17 16:51:38 -0500

    adjust logo

--- src/components/DesktopAthumLogo.jsx
+++ src/components/DesktopAthumLogo.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { bool, string } from 'prop-types';
 import './AthumLogo.scss';
 
-const DesktopAthumLogo = ({
-  loading,
-  error,
-  hide,
-  blur,
-  showTabletPortrait,
-  img
-}) => (
+const DesktopAthumLogo = ({ loading, error, hide, blur, img }) => (
   <div
     className={`athum-logo${loading || error || hide ? 'hide' : ''} ${blur &&
-      'blur'} ${showTabletPortrait ? 'd-md-block' : 'd-md-none'}`}
+      'blur'}`}
   >
     <div className="icon">
       <img src={img} alt="builder logo" />
@@ -25,7 +18,6 @@ DesktopAthumLogo.propTypes = {
   error: string.isRequired,
   hide: bool.isRequired,
   blur: bool.isRequired,
-  showTabletPortrait: bool.isRequired,
   img: string.isRequired
 };
 
