9742b86d881fd37dcccec190a52eba39ea167bf7
Victor M Tommasi Uribe <victor@avria.co>
2022-11-18 10:23:40 -0600

    changes fo colors

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -4,10 +4,10 @@ export const atHumInstance = () => axios.create({
     baseURL: "https://us-central1-avria-production.cloudfunctions.net/showroom_api_prod/v1",
 });
 
-export const laHausInstance = () =>axios.create({
+export const laHausInstance = () => axios.create({
     baseURL: "https://microservices.staging.lahaus.com/api/pcp/v2/residential-complex",
     headers: {
         "X-API-KEY": "d49bbbeb-cd5e-4529-b492-2e7218d443c1"
-      }
+    }
 });
 
--- src/service/availabilityService.ts
+++ src/service/availabilityService.ts
@@ -2,15 +2,15 @@ import { atHumInstance, laHausInstance } from "./Api";
 
 export const availabilityService = {
     async getProject(projectId: string) {
-        const {data:response} = await atHumInstance().get(`/projects/${projectId}/svgs`);
+        const { data: response } = await atHumInstance().get(`/projects/${projectId}/svgs`);
         return response.data;
     },
-    async getProperty(id:string){
+    async getProperty(id: string) {
         const response = await laHausInstance().get(`/${id}`);
         return response;
     },
-    async getTypologies(id:string){
-        const {data} = await laHausInstance().get(`/${id}/typologies`);
+    async getTypologies(id: string) {
+        const { data } = await laHausInstance().get(`/${id}/typologies?all=true&v2=true`);
         return data;
     }
 }
