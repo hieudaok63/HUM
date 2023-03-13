d79f8bf150d75411a0fd7eb6cb1c897a729a34aa
Andy Nguyen <hieu.nguyen@adamosoft.com>
2023-03-13 12:07:08 +0700

    fix screen white

--- src/Pages/Desktop/Desktop.tsx
+++ src/Pages/Desktop/Desktop.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { useLayoutEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Filters, AvailabilityFilters, Locations } from "../../components";
@@ -14,6 +14,7 @@ import { fetchAvailability } from "../../store/todo-actions";
 
 export const Desktop = () => {
   const svgImage = useSVGImage();
+  console.log(svgImage);
   const video = useCurrentVideo();
   const type = useCurrentType();
 
@@ -26,21 +27,7 @@ export const Desktop = () => {
     setRequested(true);
   }, [dispatch, requested]);
 
-  return !svgImage ? (
-    <Box
-      sx={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        alignContent: "center",
-        justifyContent: "center",
-        alignItems: "center",
-        justifyItems: "center",
-      }}
-    >
-      <CircularProgress sx={{ color: "#3948FF" }} />
-    </Box>
-  ) : (
+  return (
     <Stack
       sx={{
         height: {
