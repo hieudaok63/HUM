2d14956e1174fe77992302e21b455c6144207ba0
Andy Nguyen <hieu.nguyen@adamosoft.com>
2023-04-17 16:10:53 +0700

    scroll mobile

--- src/components/NavigationArrows/NavigationArrows.tsx
+++ src/components/NavigationArrows/NavigationArrows.tsx
@@ -32,7 +32,7 @@ export const NavigationArrows = ({ position, disabled }: Props) => {
       setTimeout(() => {
         const myElement: any = document.getElementById("Level1");
         myElement.scrollIntoView({ behavior: "smooth" });
-      }, 2600);
+      }, 2500);
   }, []);
 
   const prevLocation = useCallback(() => {
@@ -40,7 +40,7 @@ export const NavigationArrows = ({ position, disabled }: Props) => {
       setTimeout(() => {
         const myElement: any = document.getElementById("Level1");
         myElement.scrollIntoView({ behavior: "smooth" });
-      }, 2600);
+      }, 2500);
     const newLocation =
       currentLocation === 0 ? locations.length - 1 : currentLocation;
 
@@ -111,7 +111,7 @@ export const NavigationArrows = ({ position, disabled }: Props) => {
       setTimeout(() => {
         const myElement: any = document.getElementById("Level1");
         myElement.scrollIntoView({ behavior: "smooth" });
-      }, 2600);
+      }, 2500);
     const lastLocationIndex = locations.length - 1;
     const lastViewIndex = locations[currentLocation].views.length - 1;
     if (currentView === 1 && currentLocation === 2) {
